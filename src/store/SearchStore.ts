230ab9494a85cde0e8a5ac899f834d9d90d6acc7
hawkf07 <hawkf07@proton.me>
2022-12-02 17:53:21 +0700

    initial

---
+++ src/store/SearchStore.ts
@@ -0,0 +1,57 @@
+import React, { ChangeEvent, EventHandler } from "react";
+import create from "zustand";
+import { apiData } from "../utils/types";
+
+interface useSearchStoreType {
+  inputValue: string;
+  setInputValue: (e: ChangeEvent<HTMLInputElement>) => void;
+  fetchNasaApi: (q: string) => Promise<void>;
+  apiData: apiData | null;
+  setClearInputValue: () => void;
+  inputIsFocus: boolean;
+  setInputIsFocus: (val: boolean) => void;
+  paginateApiData: (
+    array: Array<any>,
+    start_page: number,
+    end_page: number
+  ) => void;
+  paginatedApiData: Array<any>;
+}
+export const useSearchStore = create<useSearchStoreType>((set, get) => ({
+  inputValue: "",
+  inputIsFocus: false,
+  setInputIsFocus: (val: boolean) => set({ inputIsFocus: val }),
+  apiData: null,
+  setInputValue(e: ChangeEvent<HTMLInputElement>) {
+    const { inputValue } = get();
+    console.log(inputValue);
+    return set({ inputValue: e.target.value });
+  },
+  fetchNasaApi: async (q: string) => {
+    const { inputValue } = get();
+    try {
+      if (q.length !== 0) {
+        const response = await fetch(`http://localhost:3000/data?search=${q}`);
+        const data = await response.json();
+        console.log(data);
+        return set({ apiData: data });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  setClearInputValue: () => set({ inputValue: "" }),
+  paginateApiData: (array, page_number, page_size) => {
+    const { apiData } = get();
+
+    if (apiData !== null) {
+      return set({
+        paginatedApiData: array.slice(
+          (page_number - 1) * page_size,
+          page_number * page_size
+        ),
+      });
+    }
+  },
+  paginatedApiData: [],
+}));
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#677DB7",
+        secondary: "#CE6D8B",
+      },
+    },
+  },
+  plugins: [],
+};
