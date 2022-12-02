26bb80e847ba570a5bebcf89ab822d64ff8df9a5
hawkf07 <hawkf07@proton.me>
2022-12-02 17:52:31 +0700

    feat: header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+
+interface Header {
+  title: string;
+  description: string;
+}
+
+export const Header: FC<Header> = ({ title, description }) => {
+  return (
+    <header className="mobile:prose-sm prose-lg md:prose-md dark:prose-invert">
+      <h1 className="text-center ">{title}</h1>
+    </header>
+  );
+};
