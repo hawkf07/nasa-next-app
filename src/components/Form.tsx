10c1a09ec6e55e23aac561becdfb54ff44c2303d
hawkf07 <hawkf07@proton.me>
2022-12-03 13:42:22 +0700

    feat: push user to search page if submitted

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,21 @@
 import clsx from "clsx";
+import { useRouter } from "next/router";
 import type { FC, FormHTMLAttributes, ReactNode } from "react";
 import { HTMLAttributes } from "react";
+import { useSearchStore } from "../store/SearchStore";
 
 interface Form extends FormHTMLAttributes<HTMLFormElement> {
   children: ReactNode;
 }
 export const Form: FC<Form> = ({ children }) => {
+  const router = useRouter();
+  const { inputValue } = useSearchStore();
   return (
     <form
       className={clsx("container flex flex-col gap-3 lg:place-items-center")}
       onSubmit={async (e) => {
-        submitHandler(e);
-        await fetchNasaApi(inputValue);
+        e.preventDefault();
+        router.push("/search?q=" + inputValue);
       }}
     >
       {children}
